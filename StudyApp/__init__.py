e7e618e31cca9790e9813ae506cc224efce9b0f7
mateusvega <mateusggvega@gmail.com>
2025-03-21 22:10:25 -0300

    add `blog homepage`

--- StudyApp/__init__.py
+++ StudyApp/__init__.py
@@ -17,6 +17,25 @@ login_manager.login_view = "users.login"
 login_manager.login_message = {"title": "You need a account!", "message": "Please log in to your account"}
 login_manager.login_message_category = "info"
 
+class UserView(ModelView):
+    can_delete = False
+    can_create = False
+    can_edit = True
+    column_list = ['id', 'username', 'email', 'date_created']
+
+class CategoryView(ModelView):
+    can_delete = False
+    can_create = True
+    can_edit = True
+    column_list = ['id', 'name', 'color']
+
+class BlogPostView(ModelView):
+    can_delete = False
+    can_create = True
+    can_edit = True
+    column_list = ['id', 'title', 'category_id', 'image_file']
+    form_columns = ['title', 'content', 'category_id', 'image_file']
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -40,8 +59,10 @@ def create_app(config_class=Config):
     app.register_blueprint(tools)
     app.register_blueprint(community)
 
-    from StudyApp.models import User
+    from StudyApp.models import User, Category, BlogPost
     
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(UserView(User, db.session))
+    admin.add_view(CategoryView(Category, db.session))
+    admin.add_view(BlogPostView(BlogPost, db.session))
 
     return app
--- StudyApp/community/routes.py
+++ StudyApp/community/routes.py
@@ -1,10 +1,20 @@
 from flask import render_template, Blueprint
+from StudyApp.models import *
 
 community = Blueprint("community", __name__)
 
 @community.route("/blog", methods=["GET", "POST"])
 def blog():
-    return render_template("community/blog.html", title="Blog")
+    categories = Category.query.all()
+    blogs = BlogPost.query.all()
+    return render_template("community/blog.html", title="Blog", categories=categories, blogs=blogs)
+
+@community.route("/blog/<string:category>", methods=["GET", "POST"])
+def blog_category(category):
+    categories = Category.query.all()
+    category_id = Category.query.filter_by(name=category).first().id
+    blogs = BlogPost.query.filter_by(category_id=category_id).all()
+    return render_template("community/blog.html", title="Blog", categories=categories, blogs=blogs)
 
 @community.route("/forum", methods=["GET", "POST"])
 def forum():
